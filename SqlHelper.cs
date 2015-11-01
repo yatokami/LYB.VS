bcd60d68aece4723c783a167ac2231250b594f5f
yatokami <873550063@qq.com>
2015-11-01 14:38:33 +0800

    SqlHelper.cs
    sql提取数据库

---
+++ SqlHelper.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+namespace LYB
+{
+    public static class SqlHelper
+    {
+        public static readonly string connstr = ConfigurationManager.ConnectionStrings["lybDB"].ConnectionString;
+        public static SqlConnection OpenConnection()
+        {
+            SqlConnection conn = new SqlConnection(connstr);
+            conn.Open();
+            return conn;
+        }
+        public static int ExecuteNonQuery(string cmdText,params SqlParameter[] parameters)
+        {
+            using (SqlConnection conn = new SqlConnection(connstr))
+            {
+                conn.Open();
+                return ExecuteNonQuery(conn, cmdText, parameters);
+            }
+        }
+        public static int ExecuteNonQuery(SqlConnection conn,string cmdText,params SqlParameter[] parameters)
+        {
+            using(SqlCommand cmd=conn.CreateCommand())
+            {
+                cmd.CommandText = cmdText;
+                cmd.Parameters.AddRange(parameters);
+                return cmd.ExecuteNonQuery();
+            }
+        }
+        public static object ExecuteScalar(string cmdText, params SqlParameter[] parameters)
+        {
+            using(SqlConnection conn=new SqlConnection(connstr))
+            {
+                conn.Open();
+                return ExecuteScalar(conn, cmdText, parameters);
+            }
+        }
+        public static object ExecuteScalar(SqlConnection conn,string cmdText,params SqlParameter[] parameters)
+        {
+            using(SqlCommand cmd=conn.CreateCommand())
+            {
+                cmd.CommandText = cmdText;
+                cmd.Parameters.AddRange(parameters);
+                return cmd.ExecuteScalar();
+            }
+        }
+        public static DataTable ExecuteDataTable(string cmdText,params SqlParameter[] parameters)
+        {
+            using(SqlConnection conn=new SqlConnection(connstr))
+            {
+                conn.Open();
+                return ExecuteDataTable(conn, cmdText, parameters);
+            }
+        }
+        public static DataTable ExecuteDataTable(SqlConnection conn,string cmdText,params SqlParameter[] parameters)
+        {
+            using(SqlCommand cmd=conn.CreateCommand())
+            {
+                cmd.CommandText = cmdText;
+                cmd.Parameters.AddRange(parameters);
+                using(SqlDataAdapter da=new SqlDataAdapter(cmd))
+                {
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    return dt;
+                }
+            }
+        }
+    }
+}
